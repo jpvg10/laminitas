3bb61e0e97f9f7a5cd1d56a6b3ebf48846dd6c4a
Juan Pablo Valencia <juan.valencia10a@gmail.com>
2022-08-14 23:28:05 +0300

    adding prettier, tailwind and components folder

---
+++ src/components/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+const App: React.FC = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
+);
+export default App;
