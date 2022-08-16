003683c0eb0ad373a4751b97382e03c4f23ea94f
Juan Pablo Valencia <juan.valencia10a@gmail.com>
2022-08-16 23:12:57 +0300

    improving navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Navbar from './Navbar';
 
 const App: React.FC = () => (
   <Router>
-    <div className="container mx-auto px-5">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <Navbar />
+    <main className="relative">
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </main>
   </Router>
 );
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => (
+  <nav className="bg-blue-500 sticky top-0 z-50 py-4 w-full mb-5">
+    <ul className="flex justify-center text-white font-bold">
+      <li className="mr-5">
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
